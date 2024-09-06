b7362f2a496b5be04b8c4ccc3c7c55aac8a77e08
Monil Panchal <130303843+Monilp18@users.noreply.github.com>
2024-09-06 19:50:06 +0530

    FS Using Nand

---
+++ Day-4 Half and Full Adder Half and Full Subtractor Using NAND Gate/Full Subtractor Using Nand Gate/tb.v
@@ -0,0 +1,41 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04.09.2024 17:33:36
+// Design Name: 
+// Module Name: tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tb;
+reg a,b,bin;
+wire diff,bout;
+
+fs_nand uut(a,b,bin,diff,bout);
+
+initial
+    begin
+    a=0;b=0;bin=0; #10
+    a=0;b=0;bin=1; #10
+    a=0;b=1;bin=0; #10
+    a=0;b=1;bin=1; #10
+    a=1;b=0;bin=0; #10
+    a=1;b=0;bin=1; #10
+    a=1;b=1;bin=0; #10
+    a=1;b=1;bin=1; #10
+    $finish;
+end    
+endmodule
